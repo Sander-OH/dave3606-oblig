62d76b28cfd1ceb0dc53d96644ab80d5b9806b5b
Håvard G <nirentos@gmail.com>
2026-03-20 11:40:28 +0100

    Byttet string med list
    Den orginale bruken av en string variabel som man utvider vil ha en tidsbruk tilnærmet O(n^2) ettersom man i python vil lage en ny string hver gang man utvider variablen.
    Ved å bruke list, append og join reduserer man tidsbruken til O(n)

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def index():
 @app.route("/sets")
 def sets():
     template = open("templates/sets.html").read()
-    rows = ""
+    rows_list = []
 
     start_time = perf_counter()
     conn = psycopg.connect(**DB_CONFIG)
@@ -34,8 +34,14 @@ def sets():
             for row in cur.fetchall():
                 html_safe_id = html.escape(row[0])
                 html_safe_name = html.escape(row[1])
-                existing_rows = rows
-                rows = existing_rows + f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n'
+
+                rows_list.append(
+                    f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td>'
+                    f'<td>{html_safe_name}</td></tr>\n'
+                )
+
+        rows = "".join(rows_list)
+
         print(f"Time to render all sets: {perf_counter() - start_time}")
     finally:
         conn.close()
